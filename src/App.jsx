7f5f3797fa8f3633addfe4850c479ef66f306516
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-05-31 04:14:50 -0600

    sort styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Container from "./components/Container/Container";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
-import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
+import Options from "./components/Options/Options";
 
 function App() {
   const [feedback, setFeedback] = useState({
